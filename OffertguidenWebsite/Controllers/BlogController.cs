a10f6c4f8a9c8036b255b23d70475aad38b48bf6
Tobias Tunedal <Tunedal.Tobias@hotmail.com>
2017-11-16 15:39:48 +0100

    Stor Logga osv

---
+++ OffertguidenWebsite/Controllers/BlogController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using UsingAzureDB;
+using OffertguidenWebsite.Model;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+//namespace OffertguidenWebsite.Controllers
+namespace UsingAzureDB
+{
+    public class BlogController : Controller
+    {
+
+        private SqlConnection con;
+        //To Handle connection related activities
+        private void Connection()
+        {
+            string constr = ConfigurationManager.ConnectionStrings["getconn"].ToString();
+            con = new SqlConnection(constr);
+
+        }
+        // GET: /<controller>/
+        public IActionResult CreateBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateBlog(BlogModel blog)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    if (CreateBlog(blog)
+                    {
+                        ViewBag.Message = "Employee details added successfully";
+                    }
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //To Add Employee details
+        public bool CreateBlog(BlogModel obj)
+        {
+
+            Connection();
+            SqlCommand com = new SqlCommand("AddNewBlogDetails", con)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            com.Parameters.AddWithValue("@Title", obj.Title);
+            com.Parameters.AddWithValue("@Text", obj.Text);
+            com.Parameters.AddWithValue("@Date", obj.Date);
+            com.Parameters.AddWithValue("@Picture", obj.Picture);
+            con.Open();
+            int i = com.ExecuteNonQuery();
+            con.Close();
+            if (i >= 1)
+            {
+
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+            }
+        }
+    }
+}
---
+++ OffertguidenWebsite/Model/BlogModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OffertguidenWebsite.Model
+{
+    
+    public class BlogModel
+    {
+        [Key]
+
+        [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
+        public int BlogId { get; set; }
+
+        [Required(ErrorMessage = "Title is required.")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Text is required.")]
+        public string Text { get; set; }
+
+        [Required(ErrorMessage = "Date is required.")]
+        public string Date { get; set; }
+
+        public string Picture { get; set; }
+    }
+}
